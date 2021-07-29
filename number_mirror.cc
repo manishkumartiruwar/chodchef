97d9b5b14065a13ab26edb8a4d8319280034705b
Manish Kumar Tiruwar <53038098+manishkumartiruwar@users.noreply.github.com>
2021-07-29 21:32:39 +0530

    Create number_mirror.cc

---
+++ number_mirror.cc
@@ -0,0 +1,27 @@
+/*
+Problem Statement
+Write a program that accepts a number, n, and outputs the same.
+
+Input
+The only line contains a single integer.
+
+Output
+Output the answer in a single line.
+
+Constraints
+0 ≤ n ≤ 105
+Sample Input
+123
+Sample Output
+123
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+	int positive_integer;
+	cin>>positive_integer;
+	cout<<positive_integer; 
+	return 0;
+}
+
