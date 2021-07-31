408d2d69e02de580412ce2ee1e68c5475799f1b4
Manish Kumar Tiruwar <53038098+manishkumartiruwar@users.noreply.github.com>
2021-07-31 23:55:00 +0530

    Update small_factorials.cc

--- small_factorials.cc
+++ small_factorials.cc
@@ -22,3 +22,22 @@ Sample output:
 6
 */
 
+#include<bits/stdc++.h>
+#include <boost/multiprecision/cpp_int.hpp> 
+using namespace boost::multiprecision;
+using namespace std;
+
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+		cin>>n;
+		cpp_int fact = 1;
+		for(int i=n;i>0;i--){
+	        fact=fact*i;
+	    }
+	    cout << fact << endl;
+	}
+return 0;
+}
